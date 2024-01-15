775bdca1feb4915c6e46426da087cf0dd7eb3b1c
ran <ranwurembrand@gmail.com>
2024-01-15 17:35:14 +0200

    first step made,including dummy files,tests,Cmake

---
+++ src/calc.cpp
@@ -0,0 +1,8 @@
+//
+// Created by ran on 1/15/24.
+//
+
+
+int sum(int a, int b) {
+    return a - b;
+}
---
+++ tests/calc_test.cpp
@@ -0,0 +1,6 @@
+#include <gtest/gtest.h>
+#include "../src/calc.cpp"
+
+TEST(SumTest, BasicTest){
+    EXPECT_EQ(sum(1,2),3);
+}
