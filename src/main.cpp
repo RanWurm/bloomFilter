bd87f0fe55321f991f6feffef9f14a0ada5acaec
ran <ranwurembrand@gmail.com>
2024-01-24 14:38:38 +0200

    main added

---
+++ src/main.cpp
@@ -0,0 +1,17 @@
+//
+// Created by ran on 1/23/24.
+//
+
+#include "App.cpp"
+#include "dumAuthinticator.cpp"
+#include "DataProcessor.cpp"
+#include "BloomFilter.cpp"
+#include "StandardHash.cpp"
+#include "calc.cpp"
+int main(){
+    App a = App();
+    a.run();
+
+
+    return 0;
+}
